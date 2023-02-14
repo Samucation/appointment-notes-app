56c4dd5f6aced5898a5f0ba82c40ddad0077b195
samuel.duarte <samuel_fx@hotmail.com>
2023-02-14 18:41:20 -0300

    Ajustes no código para melhorar a performace

--- src/main/java/com/lambda/appointment/notes/controller/GoogleAuthController.java
+++ src/main/java/com/lambda/appointment/notes/controller/GoogleAuthController.java
@@ -20,31 +20,31 @@ public class GoogleAuthController {
 
 
     @GET
-    @Path("/")
+    @Path("/login-url")
     public GetGoogleAuthUrlResponse redirectToGoogleAuth() {
         return googleAuthResponseGenerator.getGoogleAuthUrl();
     }
 
     @GET
-    @Path("code")
+    @Path("extract-url-code")
     public CodeExtractedFromGoogleUrlResponse getGoogleUserCode(@QueryParam("urlWithCode") String urlWithCode) {
         return googleAuthResponseGenerator.extractCodeFromRedirectUrl(urlWithCode);
     }
 
     @POST
-    @Path("callback")
+    @Path("generate-token")
     public Map<String, Object> googleCallback(@QueryParam("code") String loginAuthenticateCode) {
         return googleAuthResponseGenerator.loginGoogleAccess(loginAuthenticateCode);
     }
 
     @POST
-    @Path("user")
+    @Path("user-values")
     public GoogleUserIdResponse getGoogleUserParams(@QueryParam("accessToken") String accessToken) {
         return googleAuthResponseGenerator.getGoogleUserParams(accessToken);
     }
 
     @POST
-    @Path("valid-token")
+    @Path("token-is-valid")
     public IsGoogleTokenValidResponse isTokenValid(@QueryParam("accessToken") String accessToken) {
         return googleAuthResponseGenerator.isTokenValid(accessToken);
     }
